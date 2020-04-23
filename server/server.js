0795566c69029dc24d8dc04ece0cb5000998594e
Jarvis Yang <jarvis.yang@prim.com>
2020-04-23 08:27:56 -0500

    added dotenv package to pull in env locally

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
+require('dotenv').config();
 const bodyParser = require('body-parser');
-const pg = require('pg');
 // const cors = require('cors');
 
 const port = process.env.PORT || 5000;
